d4e69347949f9a2508a1f1dca0953af731448aa0
贾一饼 <lby.i@qq.com>
2025-12-12 17:36:25 +0800

    fix: response

--- types/response/list.go
+++ types/response/list.go
@@ -1,7 +1,13 @@
 package response
 
 type List[T any] struct {
+	List  []T  `json:"list"`
+	Total uint `json:"total,omitempty"`
+}
+
+type CursorList[T, ID any] struct {
 	List    []T  `json:"list"`
 	Total   uint `json:"total,omitempty"`
+	Cursor  ID
 	HasMore bool `json:"hasMore"`
 }
